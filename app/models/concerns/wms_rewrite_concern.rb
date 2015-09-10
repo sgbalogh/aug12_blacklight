d82e0f6f3501bdcedcaf63cad6cd383ace41df23
Ubuntu <sgbalogh@gmail.com>
2015-09-10 19:09:33 +0000

    appends '?' to resource to make WMS request work' ;

--- app/models/concerns/wms_rewrite_concern.rb
+++ app/models/concerns/wms_rewrite_concern.rb
@@ -1,20 +1,21 @@
 module WmsRewriteConcern
-#  extend Geoblacklight::SolrDocument
-#
-#  def viewer_endpoint
-#    if is_nyu_restricted?
-#      # replace wms prefix with webauthed proxy
-#      super.prepend(Settings.PROXY_URL)
-#    else
-#      super
-#    end
-#  end
-#
-#  def is_nyu_restricted?
-#    nyu? && restricted?
-#  end
-#
-#  def nyu?
-#    fetch(:dct_provenance_s).downcase == 'nyu'
-#  end
+  extend Geoblacklight::SolrDocument
+
+  def viewer_endpoint
+    if is_nyu_restricted?
+      # replace wms prefix with webauthed proxy
+      super.prepend(Settings.PROXY_URL)
+      super.concat('?')
+    else
+      super
+    end
+  end
+
+  def is_nyu_restricted?
+    nyu? && restricted?
+  end
+
+  def nyu?
+    fetch(:dct_provenance_s).downcase == 'nyu'
+  end
 end
