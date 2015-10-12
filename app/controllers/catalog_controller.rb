a0c9ae30d4b7e185dab7dcb58fe5d5272414d763
Ubuntu <sgbalogh@gmail.com>
2015-10-12 19:18:05 +0000

    adds link to geom type

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -125,6 +125,7 @@ class CatalogController < ApplicationController
     config.add_show_field 'dct_spatial_sm', label: 'Place(s)', itemprop: 'spatial', link_to_search: true
     config.add_show_field 'dc_subject_sm', label: 'Subject(s)', itemprop: 'keywords', link_to_search: true
     config.add_show_field 'dct_temporal_sm', label: 'Year', itemprop: 'temporal', link_to_search: true
+    config.add_show_field 'layer_geom_type_s', label: 'Layer Geometry', itemprop: 'geometry', link_to_search: true
     config.add_show_field 'dct_provenance_s', label: 'Held by', link_to_search: true
 
     # "fielded" search configuration. Used by pulldown among other places.
