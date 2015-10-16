6be9d6fff63921aba146cc4ffe75be7893d49a2c
Ubuntu <sgbalogh@gmail.com>
2015-10-16 20:11:06 +0000

    adds new_session_path function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   include Blacklight::Controller
   layout 'blacklight'
 
+# Alias new_session_path as login_path for default devise config
+def new_session_path(scope)
+  login_path
+end
+
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
