2b8e963ce028634bc84a9fbffb37a7f8c7268872
Ubuntu <sgbalogh@gmail.com>
2015-10-16 19:56:11 +0000

    Add username index to User table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016195334) do
+ActiveRecord::Schema.define(version: 20151016195520) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       null: false
@@ -58,5 +58,6 @@ ActiveRecord::Schema.define(version: 20151016195334) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username", "provider"], name: "index_users_on_username_and_provider", unique: true
 
 end
