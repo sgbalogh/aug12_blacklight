52c7743bfe5dd30dba9706423b1db09a420e7d85
Ubuntu <sgbalogh@gmail.com>
2015-10-16 19:51:06 +0000

    Add new user fields to User table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150812160637) do
+ActiveRecord::Schema.define(version: 20151016194919) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       null: false
@@ -49,6 +49,10 @@ ActiveRecord::Schema.define(version: 20150812160637) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "guest",                  default: false
+    t.string   "provider",               default: "",    null: false
+    t.string   "aleph_id"
+    t.string   "institution_code"
+    t.string   "patron_status"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
