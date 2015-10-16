0c4c404be6fba7746be27b46985520a4969f1a2e
Ubuntu <sgbalogh@gmail.com>
2015-10-16 19:55:07 +0000

    adds username column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016194919) do
+ActiveRecord::Schema.define(version: 20151016195334) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20151016194919) do
     t.string   "aleph_id"
     t.string   "institution_code"
     t.string   "patron_status"
+    t.string   "username",               default: "",    null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
